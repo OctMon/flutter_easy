7429b1838e34b72053801dd7fb73ff06f68f0cce
OctMon <octmon@qq.com>
2019-08-17 23:39:28 +0800

    add  distance

--- lib/utils/distance_utils.dart
+++ lib/utils/distance_utils.dart
@@ -1,23 +1,54 @@
 const double distanceWith1 = 1;
-
+const double distanceWith2 = 2;
+const double distanceWith3 = 3;
+const double distanceWith4 = 4;
 const double distanceWith5 = 5;
-
+const double distanceWith6 = 6;
+const double distanceWith7 = 7;
 const double distanceWith8 = 8;
-
+const double distanceWith9 = 9;
 const double distanceWith10 = 10;
-
+const double distanceWith11 = 11;
+const double distanceWith12 = 12;
+const double distanceWith13 = 13;
+const double distanceWith14 = 14;
 const double distanceWith15 = 15;
-
+const double distanceWith16 = 16;
+const double distanceWith17 = 17;
+const double distanceWith18 = 18;
+const double distanceWith19 = 19;
 const double distanceWith20 = 20;
-
+const double distanceWith21 = 21;
+const double distanceWith22 = 22;
+const double distanceWith23 = 23;
+const double distanceWith24 = 24;
+const double distanceWith25 = 25;
+const double distanceWith26 = 26;
+const double distanceWith27 = 27;
+const double distanceWith28 = 28;
+const double distanceWith29 = 29;
 const double distanceWith30 = 30;
-
+const double distanceWith35 = 35;
+const double distanceWith40 = 40;
+const double distanceWith45 = 45;
 const double distanceWith50 = 50;
-
+const double distanceWith55 = 55;
+const double distanceWith60 = 60;
+const double distanceWith65 = 65;
+const double distanceWith70 = 70;
+const double distanceWith75 = 75;
 const double distanceWith80 = 80;
-
+const double distanceWith85 = 85;
+const double distanceWith90 = 90;
+const double distanceWith95 = 95;
 const double distanceWith100 = 100;
-
+const double distanceWith110 = 110;
+const double distanceWith120 = 120;
+const double distanceWith130 = 130;
+const double distanceWith140 = 140;
 const double distanceWith150 = 150;
-
+const double distanceWith160 = 160;
+const double distanceWith170 = 170;
+const double distanceWith180 = 180;
+const double distanceWith190 = 190;
 const double distanceWith200 = 200;
