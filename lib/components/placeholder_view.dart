592a4e7035d19f832aabc3205122fcffa74f8f41
OctMon <octmon@qq.com>
2019-09-04 23:35:42 +0800

    add this.onTap

--- lib/components/placeholder_view.dart
+++ lib/components/placeholder_view.dart
@@ -7,32 +7,38 @@ import '../utils/global_util.dart';
 class PlaceholderView extends StatelessWidget {
   final String title;
   final String image;
+  final VoidCallback onTap;
+
   const PlaceholderView({
     Key key,
     this.title = '暂无数据',
     this.image = 'empty',
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: distanceWith150,
-      child: Column(
-        children: <Widget>[
-          Image.asset(
-            assetsImagesPath(image),
-            width: distanceWith100,
-            height: distanceWith100,
-          ),
-          SizedBox(height: distanceWith15),
-          BaseText(
-            title,
-            style: TextStyle(
-              color: Colors.black38,
-              fontSize: 14,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: distanceWith150,
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              assetsImagesPath(image),
+              width: distanceWith100,
+              height: distanceWith100,
             ),
-          ),
-        ],
+            SizedBox(height: distanceWith15),
+            BaseText(
+              title,
+              style: TextStyle(
+                color: Colors.black38,
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
