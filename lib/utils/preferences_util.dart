90903a2bfd7ad1668a0c0453719122424d2b015b
OctMon <octmon@qq.com>
2019-09-08 20:33:16 +0800

    add bool

--- lib/utils/preferences_util.dart
+++ lib/utils/preferences_util.dart
@@ -4,4 +4,7 @@ Future<MmkvFlutter> mmkv = MmkvFlutter.getInstance();
 Future<String> getMmkvString(String key) async => (await mmkv).getString(key);
 Future<bool> setMmkvString(String key, String value) async =>
     (await mmkv).setString(key, value);
+Future<bool> getMmkvBool(String key) async => (await mmkv).getBool(key);
+Future<bool> setMmkvBool(String key, bool value) async =>
+    (await mmkv).setBool(key, value);
 Future<bool> removeMmkvByKey(String key) async => (await mmkv).removeByKey(key);
