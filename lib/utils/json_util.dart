f2f976acc9ad57d52b6b17bc5266ff8ff9435660
OctMon <octmon@qq.com>
2020-04-01 18:22:09 +0800

    add sortMap

--- lib/utils/json_util.dart
+++ lib/utils/json_util.dart
@@ -7,3 +7,23 @@ String jsonEncode(Object object, {Object toEncodable(Object nonEncodable)}) =>
 /// Parses the string and returns the resulting Json object.
 dynamic jsonDecode(String source, {Object reviver(Object key, Object value)}) =>
     json.decode(source, reviver: reviver);
+
+Map sortMap(Map map) {
+  List keys = map.keys.toList();
+  keys.sort((a, b) {
+    List<int> al = a.codeUnits;
+    List<int> bl = b.codeUnits;
+    for (int i = 0; i < al.length; i++) {
+      if (bl.length <= i) return 1;
+      if (al[i] > bl[i]) {
+        return 1;
+      } else if (al[i] < bl[i]) return -1;
+    }
+    return 0;
+  });
+  Map newMap = {};
+  keys.forEach((e) {
+    newMap[e] = map[e];
+  });
+  return newMap;
+}
