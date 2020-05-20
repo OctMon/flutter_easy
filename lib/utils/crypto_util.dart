4072c9829b1f0524bac4685b548fae5a5bc213f4
OctMon <octmon@qq.com>
2020-05-20 15:48:35 +0800

    try catch base64Decode

--- lib/utils/crypto_util.dart
+++ lib/utils/crypto_util.dart
@@ -18,7 +18,16 @@ String base64EncodeString(String data) => base64Encode(utf8.encode(data));
 String base64Encode(List<int> input) => base64.encode(input);
 
 /// base64解密字符串
-String base64DecodeString(String data) => utf8.decode(base64Decode(data));
+String base64DecodeString(String data) {
+  Uint8List decode = base64Decode(data);
+  return decode != null ? utf8.decode(decode) : "";
+}
 
 /// base64解密字符串
-Uint8List base64Decode(String encoded) => base64.decode(encoded);
+Uint8List base64Decode(String encoded) {
+  Uint8List decode;
+  try {
+    decode = base64.decode(encoded);
+  } catch (e) {}
+  return decode;
+}
