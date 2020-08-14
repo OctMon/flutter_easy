1dd29adc2dc5d2438a08496eac5f080a2b72a79f
OctMon <octmon@qq.com>
2020-08-14 16:21:54 +0800

    createEasyApp

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,31 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easy/flutter_easy.dart';
 
 void main() {
-  runApp(MyApp());
+  createEasyApp(
+    isSelectBaseURLTypeFlag: true,
+    initCallback: initApp,
+    completionCallback: () {
+      runApp(createApp());
+      if (isAndroid) {
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]);
+        // Set overlay style status bar. It must run after MyApp(), because MaterialApp may override it.
+        SystemUiOverlayStyle systemUiOverlayStyle =
+            SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+        SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+      }
+    },
+  );
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+Future<void> initApp() async {
+  // 存储混合中的密钥
+  StorageUtil.setEncrypt("963K3REfb30szs1n");
+  // 加载用户信息
+//  await UserStore.load();
+  // 获取登录状态
+//  routesIsLogin = () => UserStore.store.getState().isLogin;
+
+  colorWithBrightness = Brightness.dark;
 }
 
-class _MyAppState extends State<MyApp> {
+Widget createApp() {
+  return BaseApp(
+    home: HomePage(),
+  );
+}
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('flutter_easy example'),
-        ),
-        body: Center(
-          child: Text('Running'),
-        ),
+    return BaseScaffold(
+      appBar: BaseAppBar(
+        title: const Text('flutter_easy example'),
+      ),
+      body: Center(
+        child: Text('Running'),
       ),
     );
   }
