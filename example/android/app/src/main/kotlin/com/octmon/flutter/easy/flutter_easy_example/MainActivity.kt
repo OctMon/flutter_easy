4d7cfe7a15c2e9108f55138c150565bf8eb25aa8
OctMon <octmon@qq.com>
2020-08-14 15:07:35 +0800

    create example

---
+++ example/android/app/src/main/kotlin/com/octmon/flutter/easy/flutter_easy_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.octmon.flutter.easy.flutter_easy_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <none.h>
+#include <none.h>
+#include <url_launcher_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  noneRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("none"));
+  noneRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("none"));
+  UrlLauncherPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+}
