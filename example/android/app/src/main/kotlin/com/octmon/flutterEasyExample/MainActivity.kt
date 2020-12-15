757650320894fba41e372eb4abd7cf6aa23bb8e5
OctMon <octmon@qq.com>
2020-12-15 15:12:39 +0800

    set multiDexEnabled true

--- example/android/app/src/main/kotlin/com/octmon/flutterEasyExample/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/octmon/flutterEasyExample/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.octmon.flutterEasyExample
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import androidx.multidex.MultiDex
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 初始化MultiDex
+        MultiDex.install(this)
+    }
+
 }
