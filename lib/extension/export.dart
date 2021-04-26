810edb1b6514d1ccca473854c6a60340a56e65bb
OctMon <octmon@qq.com>
2021-04-26 22:04:55 +0800

    Fix sortKeys

--- lib/extension/export.dart
+++ lib/extension/export.dart
@@ -2,3 +2,4 @@ export 'src/duration_extensions.dart';
 export 'src/dynamic_extensions.dart';
 export 'src/num_extensions.dart';
 export 'src/string_extensions.dart';
+export 'src/map_extensions.dart';
--- lib/extension/src/map_extensions.dart
+++ lib/extension/src/map_extensions.dart
@@ -1,24 +1,8 @@
-extension MapExtensions on Map {
+import 'dart:collection';
+
+extension MapExtensions<K, V> on Map<K, V> {
   /// 按key排序
-  Map<String, dynamic> get sortKeys {
-    List<String> allKeys = this.keys.toList();
-    allKeys.sort((a, b) {
-      List<int> aCode = a.codeUnits;
-      List<int> bCode = b.codeUnits;
-      for (int i = 0; i < aCode.length; i++) {
-        if (bCode.length <= i) return 1;
-        if (aCode[i] > bCode[i]) {
-          return 1;
-        } else if (aCode[i] < bCode[i]) {
-          return -1;
-        }
-      }
-      return 0;
-    });
-    var map = Map<String, dynamic>();
-    allKeys.forEach((key) {
-      map[key] = this[key];
-    });
-    return map;
+  Map<K, V> get sortKeys {
+    return SplayTreeMap<K, V>.from(this);
   }
 }
