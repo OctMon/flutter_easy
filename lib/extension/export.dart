225e28d97b893b165607f5661fa4b60d2efe8be7
OctMon <octmon@qq.com>
2023-06-20 11:42:50 +0800

    Add function_extensions.dart

--- lib/extension/export.dart
+++ lib/extension/export.dart
@@ -1,5 +1,6 @@
 export 'src/duration_extensions.dart';
 export 'src/dynamic_extensions.dart';
+export 'src/function_extensions.dart';
 export 'src/num_extensions.dart';
 export 'src/string_extensions.dart';
 export 'src/map_extensions.dart';
