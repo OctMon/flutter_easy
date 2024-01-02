5ae4d32e1c7cd322ca5f7e5ba418210c906afe8c
OctMon <octmon@qq.com>
2024-01-02 15:17:52 +0800

    Add font extensions

--- example/lib/modules/example/connectivity/view.dart
+++ example/lib/modules/example/connectivity/view.dart
@@ -12,10 +12,78 @@ class ConnectivityPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseScaffold(
       appBar: BaseAppBar(),
-      body: Center(
-        child: Obx(() {
-          return Text("${logic.result.value}");
-        }),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Obx(() {
+            return Text("${logic.result.value}");
+          }),
+          SizedBox(height: 30),
+          Text(
+            "中文字体 Thin = w100",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: fontWeightThin,
+            ),
+          ),
+          Text(
+            "中文字体 ExtraLight = w200",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: fontWeightExtraLight,
+            ),
+          ),
+          Text(
+            "中文字体 Light = w300",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: fontWeightLight,
+            ),
+          ),
+          Text(
+            "中文字体 Regular = w400",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: fontWeightRegular,
+            ),
+          ),
+          Text(
+            "中文字体 Medium = w500",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: fontWeightMedium,
+            ),
+          ),
+          Text(
+            "中文字体 Semi-bold = w600",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: fontWeightSemiBold,
+            ),
+          ),
+          Text(
+            "中文字体 Bold = w700",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: fontWeightBold,
+            ),
+          ),
+          Text(
+            "中文字体 Extra-bold = w800",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: fontWeightBlack,
+            ),
+          ),
+          Text(
+            "中文字体 Black = w900",
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/extension/src/font_extensions.dart
@@ -0,0 +1,57 @@
+import 'dart:ui';
+
+/// `FontWeight.w100`
+const FontWeight fontWeightThin = BaseFontWeight.thin;
+
+/// `FontWeight.w200`
+const FontWeight fontWeightExtraLight = BaseFontWeight.extraLight;
+
+/// `FontWeight.w300`
+const FontWeight fontWeightLight = BaseFontWeight.light;
+
+/// `FontWeight.w400`
+const FontWeight fontWeightRegular = BaseFontWeight.regular;
+
+/// `FontWeight.w500`
+const FontWeight fontWeightMedium = BaseFontWeight.medium;
+
+/// `FontWeight.w600`
+const FontWeight fontWeightSemiBold = BaseFontWeight.semiBold;
+
+/// `FontWeight.w700`
+const FontWeight fontWeightBold = BaseFontWeight.bold;
+
+/// `FontWeight.w800`
+const FontWeight fontWeightExtraBold = BaseFontWeight.extraBold;
+
+/// `FontWeight.w900`
+const FontWeight fontWeightBlack = BaseFontWeight.black;
+
+extension BaseFontWeight on FontWeight {
+  /// `FontWeight.w100`
+  static const FontWeight thin = FontWeight.w100;
+
+  /// `FontWeight.w200`
+  static const FontWeight extraLight = FontWeight.w200;
+
+  /// `FontWeight.w300`
+  static const FontWeight light = FontWeight.w300;
+
+  /// `FontWeight.w400`
+  static const FontWeight regular = FontWeight.w400;
+
+  /// `FontWeight.w500`
+  static const FontWeight medium = FontWeight.w500;
+
+  /// `FontWeight.w600`
+  static const FontWeight semiBold = FontWeight.w600;
+
+  /// `FontWeight.w700`
+  static const FontWeight bold = FontWeight.w700;
+
+  /// `FontWeight.w800`
+  static const FontWeight extraBold = FontWeight.w800;
+
+  /// `FontWeight.w900`
+  static const FontWeight black = FontWeight.w900;
+}
