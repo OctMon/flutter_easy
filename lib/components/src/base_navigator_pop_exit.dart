82e9e1a6f4f2361a1f233696db2676c97b8783aa
OctMon <octmon@qq.com>
2024-06-24 19:55:07 +0800

    feat: add BaseNavigatorPopExit

---
+++ lib/components/src/base_navigator_pop_exit.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easy/flutter_easy.dart';
+
+class BaseNavigatorPopExit extends StatelessWidget {
+  final Widget child;
+  final Duration? duration;
+
+  const BaseNavigatorPopExit({super.key, required this.child, this.duration});
+
+  @override
+  Widget build(BuildContext context) {
+    /// 上次点击的时间
+    DateTime? lastPressedAt;
+
+    return BasePopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        if (lastPressedAt == null ||
+            (DateTime.now().difference(lastPressedAt!) >
+                (duration ?? const Duration(seconds: 1)))) {
+          // 两次点击间隔超过1秒，重新计时
+          lastPressedAt = DateTime.now();
+          return;
+        }
+        SystemNavigator.pop();
+      },
+      child: child,
+    );
+  }
+}
