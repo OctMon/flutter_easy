71a9773e95e641117a1b75e7c0e5745e96e1a9eb
OctMon <OctMon@users.noreply.github.com>
2025-02-17 12:02:38 +0000

    deploy: 33e5755d15e0d259d1462d65a0de55d7ecc2ff14

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2083822294"
+    serviceWorkerVersion: "3733106315"
   }
 });
