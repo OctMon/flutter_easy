9d97355fe074313d731190683c1c7892cf20f44e
OctMon <OctMon@users.noreply.github.com>
2025-04-30 11:19:28 +0000

    deploy: 7e3a5cc7923c6b0a50b6b3e92f23b83b8b7be54d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -594,7 +594,7 @@ const RESOURCES = {"assets/AssetManifest.json": "d1f090c256d40f0cc95b5cb11c07bb9
 "canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
 "index.html": "1ac81794048838bb3c426e7f95db7531",
 "/": "1ac81794048838bb3c426e7f95db7531",
-"flutter_bootstrap.js": "da32fb6fc883b6e278db6c0ef678afea",
+"flutter_bootstrap.js": "d919e8d678ad50c8c572dd6363c1b28f",
 "icons/Icon-192.png": "6244728c76e26c8c7dd601195eda9e41",
 "icons/Icon-maskable-192.png": "6244728c76e26c8c7dd601195eda9e41",
 "icons/Icon-maskable-512.png": "8a83f509d98296a3e7131ebb955ef8e9",
